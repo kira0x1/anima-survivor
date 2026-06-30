309e5391f68d6aff3c31a00bceafd1340afdcb69
Kira <kira.eva@protonmail.com>
2026-06-30 18:48:32 +0100

    slot label

--- scripts/actionbar_slot.gd
+++ scripts/actionbar_slot.gd
@@ -2,6 +2,13 @@ extends Button
 
 var is_occupied: bool = false
 var slot_item
+var slot_id: int
+var slot_text: Label
+
+func _ready() -> void:
+	slot_text = $slot_text
+	slot_id = get_index()
+	slot_text.text = "%d" % (slot_id + 1)
 
 func SetItem(item):
 	slot_item = item
