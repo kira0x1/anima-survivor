5ad8076c960d51887bc8ae455ca8b84995e9af07
Kira <kira.eva@protonmail.com>
2026-07-01 13:15:19 +0100

    create stats ui keybind and toggle

--- scripts/stats_ui.gd
+++ scripts/stats_ui.gd
@@ -6,3 +6,10 @@ extends Panel
 func _ready() -> void:
 	stamina_label.text = "stamina: %d" % %Player.stats.stamina
 	speed_label.text = "speed: %d" % %Player.stats.speed
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("stats_ui"):
+		toggle_ui()
+
+func toggle_ui() -> void:
+	visible = !visible
