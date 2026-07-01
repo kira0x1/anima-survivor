17498dff36e5b749965ed72f009dfd2e762aed9b
Kira <kira.eva@protonmail.com>
2026-07-01 07:37:32 +0100

    optimize equip spawning

--- scripts/hand_equip.gd
+++ scripts/hand_equip.gd
@@ -2,13 +2,33 @@ extends Marker2D
 
 var item_spawned: Item
 var has_equip: bool = false
+var has_spawned_item: bool = false
 
+var slot_selected_id: int = -1
+
+func hide_item():
+	item_spawned.set("visible", false)
+	
+func show_item():
+	item_spawned.set("visible", true)
 
 func _on_actionbar_slot_changed(slot: ActionbarSlot) -> void:
+
 	if slot.is_occupied:
-		var new_item: Node = slot.slot_item.item_scene.instantiate()
-		add_child(new_item)
-		item_spawned = new_item
-		has_equip = true
-	else:
+		if has_spawned_item:
+			if item_spawned.item_name != slot.slot_item.item_name:
+				item_spawned.queue_free()
+				has_spawned_item = false
+			elif item_spawned.item_name == slot.slot_item.item_name:
+				show_item()
+		else:
+			var new_item: Node = slot.slot_item.item_scene.instantiate()
+			add_child(new_item)
+			item_spawned = new_item
+			has_spawned_item = true
+			has_equip = true
+
+	elif has_spawned_item:
+		hide_item()	
 		has_equip = false
+		
