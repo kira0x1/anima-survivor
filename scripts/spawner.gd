32f417a54ff04fa7ed327697a70ae9f69d7a2973
Kira <kira.eva@protonmail.com>
2026-07-08 19:44:09 +0100

    update spawner timing
    - randomized spawn timers
    - only spawn when spawners mob have been killed under MAX_MOBS_ALIVE

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -4,18 +4,27 @@ signal on_mob_spawned
 
 # can disable spawning mobs from spawning_manager
 var is_spawning_mobs: bool = true
-const spawn_rate: float = 0.4;
 
+var min_spawn_rate: float = 0.7
+var max_spawn_rate: float = 1.8
+var spawn_rate: float = 0.4;
+
+const MAX_MOBS_ALIVE: int = 3
+var mobs_spawned_count: int = 0
 var spawn_points: Array[Variant] = []
+@onready var timer: Timer = get_node("Timer") 
 
 func _ready() -> void:
-	var timer: Timer = get_node("Timer")
 	spawn_points = find_children("spawn_*")
 	timer.wait_time = spawn_rate
 	timer.start()
 
 func _on_timer_timeout() -> void:
-	if is_spawning_mobs:
+	spawn_rate = randf_range(min_spawn_rate, max_spawn_rate)
+	timer.wait_time = spawn_rate
+	timer.start()
+		
+	if is_spawning_mobs and mobs_spawned_count <= MAX_MOBS_ALIVE:
 		spawn_mob()
 
 func spawn_mob():
@@ -24,8 +33,10 @@ func spawn_mob():
 	var mob: Mob = mob_prefab.instantiate()
 	spawn_picked.add_child(mob)
 	mob.on_death.connect(mob_died)
-	mob.global_position = spawn_picked.global_position
+	mob.global_position = spawn_picked.global_position	
 	on_mob_spawned.emit()
+	mobs_spawned_count = mobs_spawned_count + 1
 	
 func mob_died(_mob: Mob):
+	mobs_spawned_count = mobs_spawned_count - 1
 	pass
