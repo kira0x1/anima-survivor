88ee48558acc03e8bea207f0f31487c652101f36
Kira <kira.eva@protonmail.com>
2026-07-08 20:35:46 +0100

    update loot table
    loot table drops take into account item chances

--- scripts/loot table/loot_table.gd
+++ scripts/loot table/loot_table.gd
@@ -4,10 +4,22 @@ class_name LootTable
 
 @export var items_table: Array[LootTableItem] = []
 
+func generate_loot() -> Array[LootTableItem]:
+	var drops: Array[LootTableItem] = []
+
+	for item in items_table:
+		var f: float = randf()
+#		print("rng: %.2f / %.2f" % [f, item.chance])
+		if item.chance >= f:
+			drops.push_front(item)
+	
+	return drops
+			
+
 func spawn_item():
-	var picked_item: LootTableItem = items_table.pick_random()
-	var item_spawn: Node2D = picked_item.item_data.instantiate()
-#	add_child(item_spawn)
-	call_deferred("add_child", item_spawn)
-	item_spawn.set_deferred("global_position", owner.global_position)
-#	item_spawn.global_position = owner.global_position
+	var drops: Array[LootTableItem] = generate_loot()
+	
+	for item in drops:
+		var item_spawn: Node2D = item.item_data.instantiate()
+		call_deferred("add_child", item_spawn)
+		item_spawn.set_deferred("global_position", owner.global_position)
